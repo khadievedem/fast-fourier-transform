49ccb34d99eb0d184be11b1a51016c41d49f894f
Khadiev Edem <khadiev.edem@gmail.com>
2024-01-26 08:10:34 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <complex.h>
+#include <stdint.h>
+#include <stdio.h>
+
+
+double ddft(double x)
+{
+
+  return 0.0;
+}
+
+
+
+int32_t main(int argc, char *argv[])
+{
+  int32_t i = 0;
+  double complex a = 1.0 + 3.0 * I;
+
+
+  return 0;
+}
